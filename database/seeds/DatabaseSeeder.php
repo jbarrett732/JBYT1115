63e522b3688ae9158083db024cecc2be1abcd4af
John Barrett <jbarrett732@gmail.com>
2015-11-06 00:35:53 -0500

    DB config, migrations, and seeds
    Configured postgresql database, created migrations to create data
    tables, created seed classes to seed data tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,52 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(GroupsTableSeeder::class);
+        $this->command->info('Groups table seeded!');
+
+        $this->call(UsersTableSeeder::class);
+        $this->command->info('Users table seeded!');
 
         Model::reguard();
+    }
+}
+
+class GroupsTableSeeder extends Seeder
+{
+    public function run()
+    {
+	DB::table('groups')->insert([
+	    [ 
+		'name' => 'adminUser',   
+		'created_at' => \Carbon\Carbon::now()->toDateTimeString(),   
+		'updated_at' => \Carbon\Carbon::now()->toDateTimeString()   
+	    ],
+	    [ 
+		'name' => 'regularUser', 
+		'created_at' => \Carbon\Carbon::now()->toDateTimeString(),   
+		'updated_at' => \Carbon\Carbon::now()->toDateTimeString()   
+	    ]
+	]);
+    }
+}
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+	DB::table('users')->insert([
+	    [
+		'name' => 'adminUser',
+		'password' => 'supersecret1',
+		'created_at' => \Carbon\Carbon::now()->toDateTimeString(),   
+		'updated_at' => \Carbon\Carbon::now()->toDateTimeString()   
+    	    ],
+	    [
+		'name' => 'regularUser',
+		'password' => 'secret1',
+		'created_at' => \Carbon\Carbon::now()->toDateTimeString(),   
+		'updated_at' => \Carbon\Carbon::now()->toDateTimeString()   
+	    ]
+	]);
     }
 }
